9c8f4ddf213c903a2a1c828d13f3405df1de806f
Hugh Perkins <hughperkins@gmail.com>
2026-03-16 13:01:13 -0400

    [Build] Split CI tests into torch and non-torch phases (#418)

--- tests/python/test_ad_ndarray_torch.py
+++ tests/python/test_ad_ndarray_torch.py
@@ -6,10 +6,9 @@ from tests import test_utils
 
 archs_support_ndarray_ad = [qd.cpu, qd.cuda]
 
-try:
-    import torch
-except ImportError:
-    pytest.skip("PyTorch not installed. Skipping...", allow_module_level=True)
+torch = pytest.importorskip("torch")
+
+pytestmark = pytest.mark.needs_torch
 
 
 @test_utils.test(arch=archs_support_ndarray_ad, default_fp=qd.f64, require=qd.extension.adstack)
