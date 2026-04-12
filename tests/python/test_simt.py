84ec64aee4dc9293fae2051a110a417b4a673007
Hugh Perkins <hughperkins@gmail.com>
2026-04-12 10:48:25 -0700

    test: run subgroup_size validation on all backends including Metal/AMDGPU

--- tests/python/test_simt.py
+++ tests/python/test_simt.py
@@ -718,7 +718,7 @@ def test_subgroup_size_range():
 
 
 @pytest.mark.parametrize("sg_size", [8, 16, 32, 64])
-@test_utils.test(arch=[qd.cpu, qd.cuda, qd.vulkan])
+@test_utils.test(arch=[qd.cpu, qd.cuda, qd.vulkan, qd.metal, qd.amdgpu])
 def test_subgroup_size_validation(sg_size):
     """For each subgroup size, check it's accepted or rejected depending on arch."""
     arch = qd.lang.impl.current_cfg().arch
