fabef3c381093ef8b7e52ad6e8f0a8ba048ed342
Alexis Duburcq <alexis.duburcq@gmail.com>
2026-04-16 12:21:37 +0200

    [AMDGPU] Declare sparse/quant/quant_basic/data64/mesh extension support.

--- quadrants/program/extension.cpp
+++ quadrants/program/extension.cpp
@@ -16,7 +16,9 @@ bool is_extension_supported(Arch arch, Extension ext) {
       {Arch::cuda,
        {Extension::sparse, Extension::quant, Extension::quant_basic, Extension::data64, Extension::adstack,
         Extension::bls, Extension::assertion, Extension::mesh}},
-      {Arch::amdgpu, {Extension::adstack, Extension::assertion}},
+      {Arch::amdgpu,
+       {Extension::sparse, Extension::quant, Extension::quant_basic, Extension::data64, Extension::adstack,
+        Extension::assertion, Extension::mesh}},
       {Arch::metal, {}},
       {Arch::vulkan, {}},
   };
