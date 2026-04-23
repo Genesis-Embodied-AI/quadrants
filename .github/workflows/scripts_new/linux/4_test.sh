bea34ecf2baafcae4a1882f787ddc572a52edb78
jamesETsmith <james.smith9113@gmail.com>
2026-04-22 21:22:59 -0400

    Add reruns back to the quadrants tests and add -e back to the test script

--- .github/workflows/scripts_new/linux/4_test.sh
+++ .github/workflows/scripts_new/linux/4_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 pip install --group test
 pip install -r requirements_test_xdist.txt
 export QD_LIB_DIR="$(python -c 'import quadrants as ti; print(ti.__path__[0])' | tail -n 1)/_lib/runtime"
 ./build/quadrants_cpp_tests
 
-python tests/run_tests.py -v -r 0 -a amdgpu -t 16
+python tests/run_tests.py -v -r 3 -a amdgpu -t 16
