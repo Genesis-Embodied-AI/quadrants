c3a8483a4214cc74146dc09245e309f834320e5d
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2026-04-27 08:20:39 +0200

    Tighter rejection criteria.

--- tests/python/test_fail_device_memory_allocation.py
+++ tests/python/test_fail_device_memory_allocation.py
@@ -5,7 +5,9 @@ import quadrants as qd
 from tests import test_utils
 
 
-@pytest.mark.skip("FIXME: This test is causing OOM error on the CI, silently crashing jobs...")
+@pytest.mark.skipif(
+    sys.platform == "darwin", reason="FIXME: This test is causing OOM error on the CI, crashing jobs with error 143..."
+)
 @test_utils.test(arch=qd.gpu)
 def test_huge_allocation_fail_at_allocate_time():
     """Ensure huge allocation fails at allocate time and not at memset to 0"""
