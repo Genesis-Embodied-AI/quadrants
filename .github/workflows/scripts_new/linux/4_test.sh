2a990fd6ecb4b32f1943d6018acb730b148d288d
James E T Smith <jamesETsmith@users.noreply.github.com>
2026-04-28 02:26:50 -0400

    Running the test_reduction_single* tests separately bc they timeout when other workers are using the gpu (#20)

--- .github/workflows/scripts_new/linux/4_test.sh
+++ .github/workflows/scripts_new/linux/4_test.sh
@@ -7,4 +7,7 @@ pip install -r requirements_test_xdist.txt
 export QD_LIB_DIR="$(python -c 'import quadrants as ti; print(ti.__path__[0])' | tail -n 1)/_lib/runtime"
 ./build/quadrants_cpp_tests
 
-python tests/run_tests.py -v -r 3 -a amdgpu -t 16
+# The test_reduction_single* tests put the GPU under a lot of stress and can run
+# out of time during the test run with lots of GPU workers. We should run them separately.
+python tests/run_tests.py -v -r 3 -a amdgpu -t 16 -k "not test_reduction_single"
+python tests/run_tests.py -v -r 3 -a amdgpu -t 16 -k "test_reduction_single"
